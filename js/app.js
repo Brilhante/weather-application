0cfd0bce34bd6b6369597d93c30b80f5dc31e7c9
Alan Brilhante <alancoutinho@sisconunica.com.br>
2023-01-26 11:24:49 -0300

    Refactory

--- js/app.js
+++ js/app.js
@@ -6,24 +6,17 @@ const cityCard = document.querySelector('[data-js="city-card"]')
 let timeImg = document.querySelector('[data-js="time"]')
 const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 
-
-cityForm.addEventListener('submit', async event => {
-    event.preventDefault()
-    const inputValue = event.target.city.value
-    const [{ Key, LocalizedName }] = await getCityData(inputValue)
-    const  [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
-
-    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
-    
+const showCityCard = () => {
     if(cityCard.classList.contains('d-none')){
         cityCard.classList.remove('d-none')
     }
+}
 
-    if (IsDayTime) {
-        timeImg.src = './src/day.svg'
-    } else {
-        timeImg.src = './src/night.svg'
-    }
+const showCityWeatherInfo = async cityName => {
+    const [{ Key, LocalizedName }] = await getCityData(cityName)
+    const  [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
+    const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg" />`
+    IsDayTime ? timeImg.src = './src/day.svg' : timeImg.src = './src/night.svg'
 
     timeIconContainer.innerHTML = timeIcon
     cityNameContainer.textContent = LocalizedName
@@ -31,5 +24,23 @@ cityForm.addEventListener('submit', async event => {
     cityWeatherContainer.textContent = WeatherText
     cityTemperatureContainer.textContent = Temperature.Metric.Value
 
+    showCityCard()
+}
+
+const showLocalStorageCity = () => {
+    const city = localStorage.getItem('city')
+
+    if (city) {
+        showCityWeatherInfo(city)
+    }
+}
+
+cityForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const inputValue = event.target.city.value
+    showCityWeatherInfo(inputValue)
+    localStorage.setItem('city', inputValue)
     cityForm.reset()
 })
+
+showLocalStorageCity()
